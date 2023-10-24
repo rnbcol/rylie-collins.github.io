e2c077313c24a47225d26d47367f33a6f1f1bd5c
rylie collins <rylie.collins@xrealm.net>
2023-10-24 21:34:24 +0000

    added lodown

---
+++ lodown/index.js
@@ -0,0 +1,23 @@
+// YOU KNOW WHAT TO DO //
+
+/**
+ * each: Designed to loop over a collection, Array or Object, and applies the action 
+ * Function to each value in the collection.
+ * each: Designed to loop over a collection, Array or Object, and applies the 
+ * action Function to each value in the collection.
+ * 
+ * @param {Array or Object} collection The collection over which to iterate.
+ * @param {Function} action The Function to be applied to each value in the 
+ * @param {Array or Object} collection: The collection over which to iterate.
+ * @param {Function} action: The Function to be applied to each value in the 
+ * collection
+ */
+function each(collection, action) {
+    @@ -21,4 +21,4 @@ function each(collection, action) {
+            }
+        }
+    }
+    module.exports.each = each;
+    module.exports.each = each;
+
+    
