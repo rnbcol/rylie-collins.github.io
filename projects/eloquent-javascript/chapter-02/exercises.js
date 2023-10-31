c843e5fa144d27651f08a5c704b0160d8b1484d5
rylie collins <rylie.collins@xrealm.net>
2023-10-31 15:32:22 +0000

    finished eloquent javascript chap 2

--- projects/eloquent-javascript/chapter-02/exercises.js
+++ projects/eloquent-javascript/chapter-02/exercises.js
@@ -40,7 +40,19 @@ function fizzBuzz() {
 // x is number of rows and 
 // use line break character /n
 function drawChessboard(x) {
-
+  let size = x;
+  let board = "";
+  for (let i = 0; i < size; i++) {
+    for (let x = 0; x < size; x++) {
+      if ((i + x) % 2 === 0) {
+        board += " ";
+      } else {
+        board += "#";
+      }
+    }
+    board += "\n";
+  }
+  console.log(board);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
